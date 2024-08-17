ff79206a9c0e9d9b4c48ca565eebbc61a813f940
wthegamer <winst1231@gmail.com>
2024-08-17 17:24:59 -0500

    Update config

--- staticSite/astro.config.mjs
+++ staticSite/astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  site: 'https://wthegamer.github.io/BibleSentiment/',
+  base: 'BibleSentiment/staticsite',
 });
