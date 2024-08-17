356e6984fa1512803ec11979eb44248065548b90
wthegamer <winst1231@gmail.com>
2024-08-17 18:51:49 -0500

    Update paths

--- staticSite/astro.config.mjs
+++ staticSite/astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
+
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://wthegamer.github.io/BibleSentiment/',
-  base: '/',
+  base: 'BibleSentiment/staticsite',
 });
